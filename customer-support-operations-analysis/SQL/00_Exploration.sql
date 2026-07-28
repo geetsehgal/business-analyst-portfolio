342375a6d2de49e8e470b77251af01b555728ed6
Geet Sehgal <66760190+geetsehgal@users.noreply.github.com>
2026-07-29 05:29:01 +0530

    Update 00_Exploration.sql

--- customer-support-operations-analysis/SQL/00_Exploration.sql
+++ customer-support-operations-analysis/SQL/00_Exploration.sql
@@ -9,3 +9,63 @@ FROM tickets;
 SELECT *
 FROM tickets
 LIMIT 10;
+
+# SQL Exploration
+
+## Database
+
+SQLite
+
+## Table
+
+tickets
+
+## Dataset Size
+
+(To be filled after Query 1)
+
+## Initial Observations
+
+### Priority Values
+
+-
+
+### Status Values
+
+-
+
+### Channels
+
+-
+
+### Products
+
+-
+
+### Customer Segments
+
+-
+
+### Subscription Types
+
+-
+
+### Regions
+
+-
+
+### Languages
+
+-
+
+### Browsers
+
+-
+
+### Operating Systems
+
+-
+
+## Next Steps
+
+Begin answering business questions using SQL.
